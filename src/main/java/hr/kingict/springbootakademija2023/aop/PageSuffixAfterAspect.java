39dba9fa59a446447e478be95e070cf995720e5c
Laura Isel Bacoka Klikic <laura.isel@gmail.com>
2023-10-21 13:47:04 +0200

    changed a log to error for example in after PageSuffixAfterAspect

--- src/main/java/hr/kingict/springbootakademija2023/aop/PageSuffixAfterAspect.java
+++ src/main/java/hr/kingict/springbootakademija2023/aop/PageSuffixAfterAspect.java
@@ -2,6 +2,7 @@ package hr.kingict.springbootakademija2023.aop;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,9 +13,9 @@ import org.springframework.stereotype.Component;
 public class PageSuffixAfterAspect {
 
     Logger logger = LoggerFactory.getLogger(PageSuffixAfterAspect.class);
-    @After(value = "execution(* hr.kingict.springbootakademija2023.service.PageSuffixService.getSuffix())")
-    private void afterPageSuffixServiceGetSuffix(JoinPoint joinPoint){
-        String result = joinPoint.getTarget().getClass().getSimpleName();
-        logger.info("afterPageSuffixServiceGetSuffix : " + result);
+    @AfterReturning(value = "execution(* hr.kingict.springbootakademija2023.service.PageSuffixService.getSuffix())",returning = "result")
+    private void afterPageSuffixServiceGetSuffix(String result){
+
+        logger.error("afterPageSuffixServiceGetSuffix : " + result);
     }
 }
