d64ee52d145622c8add0aaaf4ab4efcb94034eea
Laura Isel Bacoka Klikic <laura.isel@gmail.com>
2023-10-22 14:26:40 +0200

    added FlightSearchEntity insertment into database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,18 @@ create table test(
                      );
 
 insert into test (naziv,opis) values ('Iva','Ne radi joj punjac.');
+
+CREATE TABLE pretraga_letova(
+    id int primary key auto_increment,
+    polazni_aerodrom varchar(3),
+    dolazni_aerodrom varchar(3),
+    datum_polaska date,
+    datum_povratka date,
+    broj_putnika int,
+
+
+    korisnik_kreirao varchar(50),
+    datum_kreiranja date,
+    korisnik_azuriranja varchar (50),
+    datum_azuriranja date
+)
